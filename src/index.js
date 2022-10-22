141684f310b8878455abc667faa2791dd678a78c
LuisEJaar <luisjaarcoding@gmail.com>
2022-10-21 21:13:20 -0500

    adding basename back

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Formik>
-        <BrowserRouter>   
+      <BrowserRouter basename={process.env.public_url}>   
             <App />
         </BrowserRouter>
     </Formik>
