4d008c63453d2c8ad37b15091f2f40ac04c9829e
LuisEJaar <luisjaarcoding@gmail.com>
2022-10-21 21:16:00 -0500

    removing  modified route from app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/notthisweek" element= {<Home />} />
+        <Route path="/" element= {<Home />} />
         <Route path="/login" element= {<Login />}/>
         <Route path="/signup" element= {<Signup />} />
         <Route path="/userProfile/:id" element= {<Profile />} />
