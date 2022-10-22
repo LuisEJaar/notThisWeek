dd0b4bd55262f63328fcf78ac2aee9acf7551e8e
LuisEJaar <luisjaarcoding@gmail.com>
2022-10-22 11:15:08 -0500

    removing absolute path

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="client/" element= {<Home />} />
+        <Route path="/" element= {<Home />} />
         <Route path="/login" element= {<Login />}/>
         <Route path="/signup" element= {<Signup />} />
         <Route path="/userProfile/:id" element= {<Profile />} />
