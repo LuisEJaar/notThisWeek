48bc22f6c1e6d4b29e53a27d127b7990e3ac7a4c
LuisEJaar <luisjaarcoding@gmail.com>
2022-10-24 11:50:07 -0500

    trying with removed ensureauth

--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+// const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 // router.get("/", homeController.getIndex);
-router.get("/userProfile/:id",ensureAuth, postsController.getUserProfile);
-router.get("/gameFeed", ensureAuth, postsController.getFeed);
-router.get("/characterFeed", ensureAuth, postsController.getCharacterFeed);
+// router.get("/userProfile/:id",ensureAuth, postsController.getUserProfile);
+// router.get("/gameFeed", ensureAuth, postsController.getFeed);
+// router.get("/characterFeed", ensureAuth, postsController.getCharacterFeed);
+router.get("/userProfile/:id", postsController.getUserProfile);
+router.get("/gameFeed", postsController.getFeed);
+router.get("/characterFeed", postsController.getCharacterFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
