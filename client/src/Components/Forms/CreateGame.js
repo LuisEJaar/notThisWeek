eb0d950252954b689916abb8da1aa0bdec4831cc
LuisEJaar <luisjaarcoding@gmail.com>
2022-10-27 15:54:14 -0500

    adding create game to modal

--- client/src/Components/Forms/CreateGame.js
+++ client/src/Components/Forms/CreateGame.js
@@ -7,70 +7,68 @@ export default function CreateGame({ setGames }) {
   
   return (
     <>
-      <Formik
-        initialValues={{
-          file: null,
-          title: "",
-          caption: "",
-        }}
-        onSubmit={(values) => {
-          let formData = new FormData();
-          formData.append("file", values.file);
-          formData.append("title", values.title);
-          formData.append("caption", values.caption);
-
-          fetch(actionUrl, {
-            method: 'post',
-            encType: "multipart/form-data",
-            withCredentials: true,
-            body: formData,
-          })
-          .then((res) => res.json())
-          .then((data) => setGames(data.posts))
-          .catch((err) => {
-            console.log(err)
-          })
-        }} 
-      > 
-        {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            setFieldValue,
-            isSubmitting,
-            props
-          }) => (
-          <form
-            onSubmit={handleSubmit}
-          >
-            <TextInput handleChange={handleChange} value={values.title} id="title" text="Title:" name="title" placeholder="Interesting Title" required/>
-            {/* <div className="mb-3">
-                <label htmlFor="title" className="form-label">Title</label>
-                <input type="text" className="form-control" id="title" name="title" required/>
-            </div> */}
-
-            <Field as="textarea" onChange={handleChange} value={values.caption} className="form-control" id="gameDescription" placeholder="Game Description" name="caption"></Field>
-            
-            {/* <div className="mb-3">
-              <label htmlFor="caption" className="form-label">Description</label>
-              <textarea className="form-control" id="caption" name="caption" required></textarea>
-            </div> */}
-
-            <div className="mt-2">
-              <label htmlFor="imgUpload" className="form-label">Image</label>
-              <input type="file" onChange={ (e)=> setFieldValue("file", e.currentTarget.files[0]) } className="form-control" id="imageUpload" name="file" required/>
+      <div className="modal fade" id="createGame" tabIndex="-1" aria-labelledby="createGameLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="createGameLabel">A new journey awaits!</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            {/* <div className="mb-3">
-              <label htmlFor="imgUpload" className="form-label">Image</label>
-              <input type="file" className="form-control" id="imageUpload" name="file" required/>
-            </div> */}
-            <button type="submit" data-bs-dismiss="modal" className="btn btn-primary" value="Upload">Create Game</button>
-          </form>
-        )}
-      </Formik>
+            <div className="modal-body">
+              <Formik
+                initialValues={{
+                  file: null,
+                  title: "",
+                  caption: "",
+                }}
+                onSubmit={(values) => {
+                  let formData = new FormData();
+                  formData.append("file", values.file);
+                  formData.append("title", values.title);
+                  formData.append("caption", values.caption);
+
+                  fetch(actionUrl, {
+                    method: 'post',
+                    encType: "multipart/form-data",
+                    withCredentials: true,
+                    body: formData,
+                  })
+                  .then((res) => res.json())
+                  .then((data) => setGames(data.posts))
+                  .catch((err) => {
+                    console.log(err)
+                  })
+                }} 
+              > 
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    setFieldValue,
+                    isSubmitting,
+                    props
+                  }) => (
+                  <form
+                    onSubmit={handleSubmit}
+                  >
+                    <TextInput handleChange={handleChange} value={values.title} id="title" text="Title:" name="title" placeholder="Interesting Title" required/>
+                    <Field as="textarea" onChange={handleChange} value={values.caption} className="form-control" id="gameDescription" placeholder="Game Description" name="caption"></Field>
+                    <div className="mt-2">
+                      <label htmlFor="imgUpload" className="form-label">Image</label>
+                      <input type="file" onChange={ (e)=> setFieldValue("file", e.currentTarget.files[0]) } className="form-control" id="imageUpload" name="file" required/>
+                    </div>
+                    <button type="submit" data-bs-dismiss="modal" className="btn btn-primary" value="Upload">Create Game</button>
+                  </form>
+                )}
+              </Formik>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
+
--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -35,101 +35,91 @@ export default function Profile() {
       <Header page="other" />
       {data && 
         <>
-      <div className="container">
-        <div className="mt-5">
-          <div className="">
-            <div>
-              <p><strong>User Name</strong>: { data.targetUser.userName }</p>
+        <div className="container">
+          <div className="mt-5">
+            <div className="">
+              <div>
+                <p><strong>User Name</strong>: { data.targetUser.userName }</p>
                 <p><strong>Email</strong>: { data.targetUser.email }</p>
+              </div>
+              {(data.targetUser.type==="player" && data.visitor._id === data.targetUser._id) &&
+                <div className="mt-3">
+                  <h2>Add a character:</h2>
+                  {/* // Button Creation Modal */}
+                  <button type="button" className="shadow ms-3 btn btn-primary" data-bs-toggle="modal" data-bs-target="#createCharacter">
+                    Create New Character
+                  </button> 
+                </div>
+              }
+              {(data.targetUser.type==="dm" && data.visitor._id ===data.targetUser._id) && 
+                <div className="mt-3">
+                  <h2>Add a game:</h2>
+                  <button type="button" className="shadow ms-3 btn btn-primary" data-bs-toggle="modal" data-bs-target="#createGame">
+                    Create New Game
+                  </button>  
+                </div>
+              }
             </div>
-            {(data.targetUser.type==="player" && data.visitor._id === data.targetUser._id) &&
-              <div className="mt-3">
-                <h2>Add a character:</h2>
-                {/* // Button Creation Modal */}
-                <button type="button" className="shadow ms-3 btn btn-primary" data-bs-toggle="modal" data-bs-target="#createCharacter">
-                  Create New Character
-                </button> 
-              </div>
-            }
-            {(data.targetUser.type==="dm" && data.visitor._id ===data.targetUser._id) && 
-              <div className="mt-3">
-                <h2>Add a game:</h2>
-                <CreateGame setGames={setGames}/>
-              </div>
-            }
-          </div>
-          <div className="mt-3">
-            {(data.visitor._id !== data.targetUser._id) && 
-              <h2>Player { data.targetUser.userName }'s Games:</h2>
-            }
-            {(data.visitor._id === data.targetUser._id) && 
-              <h2>Your Games:</h2>
+            <div className="mt-3">
+              {(data.visitor._id !== data.targetUser._id) && 
+                <h2>Player { data.targetUser.userName }'s Games:</h2>
               }
-              <div className="row g-4">
-                  {games.map((game) => { 
-                    return (
-                    <div key={game._id} className="col-md-6 col-lg-3 d-flex justify-content-center">
-                        <div className="card gameDisplay" style={{ width: 18 + 'rem' }}>
-                        <Link to={`/post/${game._id}`}>
-                            <img alt="character" className="gamePicture card-img-top" src={game.image}/>
-                          </Link>
-                          <div className="card-body">
-                          <p className="card-text">{ game.title }</p>
-                          </div>
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-            {/* <ul className="row list-unstyled">
-              {games.map((game) => { 
-                return (  
-                <li key={game._id} className="justify-content-between mt-3">
-                  <Link to={`/post/${game._id}?q=proxy`}>
-                    <figure className="d-flex flex-column align-items-center">
-                      <img alt="game" className="img-fluid rounded profileGame" src={ game.image }/>
-                        <figcaption className="text-center">{game.title}</figcaption>
-                    </figure>
-                  </Link>
-                </li>
-                )
-              })}
-            </ul> */}
-          </div>
-          <div className="mt-3">
-            {(data.visitor._id !== data.targetUser._id && data.targetUser.type !=='dm') && 
-              <h2>Player { data.targetUser.userName }'s Characters:</h2>
-            }
-            {(data.visitor._id===data.targetUser._id && data.targetUser.type !=='dm') && 
-                <h2>Your Characters:</h2>
-              }
-              { data.targetUser.type !=='dm' &&
-                <>
+              {(data.visitor._id === data.targetUser._id) && 
+                <h2>Your Games:</h2>
+                }
                 <div className="row g-4">
-                  {characters.map((character) => { 
-                    return (
-                    <div key={character._id} className="col-md-6 col-lg-3 d-flex justify-content-center">
-                        <div className="card gameDisplay" style={{ width: 18 + 'rem' }}>
-                        <Link to={`/character/${character._id}`}>
-                            <img alt="character" className="gamePicture card-img-top" src={character.image}/>
-                          </Link>
-                          <div className="card-body">
-                          <p className="card-text">{ character.name }</p>
+                    {games.map((game) => { 
+                      return (
+                      <div key={game._id} className="col-md-6 col-lg-3 d-flex justify-content-center">
+                          <div className="card gameDisplay" style={{ width: 18 + 'rem' }}>
+                          <Link to={`/post/${game._id}`}>
+                              <img alt="character" className="gamePicture card-img-top" src={game.image}/>
+                            </Link>
+                            <div className="card-body">
+                            <p className="card-text">{ game.title }</p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    )
-                  })}
-                </div>
-                </>
+                      )
+                    })}
+                  </div>
+            </div>
+            <div className="mt-3">
+              {(data.visitor._id !== data.targetUser._id && data.targetUser.type !=='dm') && 
+                <h2>Player { data.targetUser.userName }'s Characters:</h2>
               }
-          </div>   
+              {(data.visitor._id===data.targetUser._id && data.targetUser.type !=='dm') && 
+                  <h2>Your Characters:</h2>
+                }
+                { data.targetUser.type !=='dm' &&
+                  <>
+                  <div className="row g-4">
+                    {characters.map((character) => { 
+                      return (
+                      <div key={character._id} className="col-md-6 col-lg-3 d-flex justify-content-center">
+                          <div className="card gameDisplay" style={{ width: 18 + 'rem' }}>
+                          <Link to={`/character/${character._id}`}>
+                              <img alt="character" className="gamePicture card-img-top" src={character.image}/>
+                            </Link>
+                            <div className="card-body">
+                            <p className="card-text">{ character.name }</p>
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </div>
+                  </>
+                }
+            </div>   
+          </div>
         </div>
-      </div>
 
-      {/* Create Character */}
-        <CreateCharacter setCharacters={ setCharacters } />
-        </>
+        {/* Create Character Modal */}
+        <CreateCharacter setCharacters={setCharacters} />
+        {/* Create Game Modal*/}
+        <CreateGame setGames={setGames}/>
+      </>
       }
       <Footer />
     </div>          
