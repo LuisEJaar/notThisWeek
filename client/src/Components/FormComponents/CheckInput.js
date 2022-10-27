eff680a29dde03e13499e8fb5c061209ef6813b1
LuisEJaar <luisjaarcoding@gmail.com>
2022-10-27 10:37:17 -0500

    adjusting checkinput with react-bootstrap

--- client/src/Components/FormComponents/CheckInput.js
+++ client/src/Components/FormComponents/CheckInput.js
@@ -1,14 +1,16 @@
+import Form from 'react-bootstrap/Form';
+
 export default function CheckInput(props) {
   return (
     <div className="form-check form-switch">
-      <div
+      <Form.Check
         value="false"
         name={props.name}
         onChange={(event, checked) => {
           props.setFieldValue(props.value, checked ? true : false);
         }}
         className="form-check-input"
-        type="checkbox"
+        type="switch"
         id={props.id}
       />
       <label className="form-check-label" htmlFor={props.id}>{ props.text }</label>
