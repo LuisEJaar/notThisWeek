b5b9a88f71ba95d6a176be34ce82208a48c0ce68
LuisEJaar <luisjaarcoding@gmail.com>
2022-10-27 14:08:32 -0500

    file didn't update?

--- client/src/Components/Forms/CreateGame.js
+++ client/src/Components/Forms/CreateGame.js
@@ -31,7 +31,7 @@ export default function CreateGame({ setGames }) {
             console.log(err)
           })
         }} 
-      >
+      > 
         {({
             values,
             errors,
